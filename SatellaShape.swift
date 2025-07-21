7f8a4ea5a356d893339dd291a845ea047066b194
pxx917144686 <111606963+pxx917144686@users.noreply.github.com>
2025-07-21 12:58:57 +0800

    Update SatellaShape.swift

--- SatellaShape.swift
+++ SatellaShape.swift
@@ -4,53 +4,75 @@ import UIKit
 final class SatellaShape: UIView {
     override func draw(_ rect: CGRect) {
         let size: CGSize = rect.size
-        let trSparkPath: UIBezierPath = .init()
-        let brSparkPath: UIBezierPath = .init()
-        let tlSparkPath: UIBezierPath = .init()
-        let blSparkPath: UIBezierPath = .init()
-        let starPath: UIBezierPath = .init()
+
+        let bodyPath = UIBezierPath(ovalIn: CGRect(
+            x: size.width * 0.3,
+            y: size.height * 0.25,
+            width: size.width * 0.4,
+            height: size.height * 0.5
+        ))
+        UIColor.systemGreen.setFill()
+        bodyPath.fill()
         
-        trSparkPath.move(to: CGPoint(x: size.width * 0.705, y: size.height * 0.295))
-        trSparkPath.addLine(to: CGPoint(x: size.width * 0.655, y: size.height * 0.195))
-        trSparkPath.addLine(to: CGPoint(x: size.width * 0.955, y: size.height * 0.045))
-        trSparkPath.addCurve(to: CGPoint(x: size.width * 0.805, y: size.height * 0.355), controlPoint1: CGPoint(x: size.width * 0.955, y: size.height * 0.045), controlPoint2: CGPoint(x: size.width * 0.805, y: size.height * 0.365))
-        trSparkPath.addCurve(to: CGPoint(x: size.width * 0.705, y: size.height * 0.295), controlPoint1: CGPoint(x: size.width * 0.805, y: size.height * 0.345), controlPoint2: CGPoint(x: size.width * 0.705, y: size.height * 0.295))
-        trSparkPath.close()
-        trSparkPath.fill()
+        let headPath = UIBezierPath(ovalIn: CGRect(
+            x: size.width * 0.35,
+            y: size.height * 0.15,
+            width: size.width * 0.3,
+            height: size.height * 0.25
+        ))
+        UIColor.systemGreen.setFill()
+        headPath.fill()
+        
+        let leftAntenna = UIBezierPath()
+        leftAntenna.move(to: CGPoint(x: size.width * 0.4, y: size.height * 0.2))
+        leftAntenna.addLine(to: CGPoint(x: size.width * 0.25, y: size.height * 0.1))
+        leftAntenna.lineWidth = 2
+        UIColor.black.setStroke()
+        leftAntenna.stroke()
+        
+        let rightAntenna = UIBezierPath()
+        rightAntenna.move(to: CGPoint(x: size.width * 0.6, y: size.height * 0.2))
+        rightAntenna.addLine(to: CGPoint(x: size.width * 0.75, y: size.height * 0.1))
+        rightAntenna.lineWidth = 2
+        UIColor.black.setStroke()
+        rightAntenna.stroke()
+        
+        for i in 0..<3 {
+            let legPath = UIBezierPath()
+            let yPos = size.height * (0.35 + Double(i) * 0.15)
+            legPath.move(to: CGPoint(x: size.width * 0.3, y: yPos))
+            legPath.addLine(to: CGPoint(x: size.width * 0.15, y: yPos + size.height * 0.05))
+            legPath.lineWidth = 2
+            UIColor.black.setStroke()
+            legPath.stroke()
+        }
+        
+        for i in 0..<3 {
+            let legPath = UIBezierPath()
+            let yPos = size.height * (0.35 + Double(i) * 0.15)
+            legPath.move(to: CGPoint(x: size.width * 0.7, y: yPos))
+            legPath.addLine(to: CGPoint(x: size.width * 0.85, y: yPos + size.height * 0.05))
+            legPath.lineWidth = 2
+            UIColor.black.setStroke()
+            legPath.stroke()
+        }
 
-        brSparkPath.move(to: CGPoint(x: size.width * 0.705, y: size.height * 0.705))
-        brSparkPath.addLine(to: CGPoint(x: size.width * 0.655, y: size.height * 0.805))
-        brSparkPath.addLine(to: CGPoint(x: size.width * 0.955, y: size.height * 0.955))
-        brSparkPath.addCurve(to: CGPoint(x: size.width * 0.805, y: size.height * 0.645), controlPoint1: CGPoint(x: size.width * 0.955, y: size.height * 0.955), controlPoint2: CGPoint(x: size.width * 0.805, y: size.height * 0.645))
-        brSparkPath.addCurve(to: CGPoint(x: size.width * 0.705, y: size.height * 0.705), controlPoint1: CGPoint(x: size.width * 0.805, y: size.height * 0.655), controlPoint2: CGPoint(x: size.width * 0.705, y: size.height * 0.705))
-        brSparkPath.close()
-        brSparkPath.fill()
-
-        tlSparkPath.move(to: CGPoint(x: size.width * 0.315, y: size.height * 0.295))
-        tlSparkPath.addLine(to: CGPoint(x: size.width * 0.365, y: size.height * 0.195))
-        tlSparkPath.addLine(to: CGPoint(x: size.width * 0.065, y: size.height * 0.045))
-        tlSparkPath.addCurve(to: CGPoint(x: size.width * 0.215, y: size.height * 0.355), controlPoint1: CGPoint(x: size.width * 0.065, y: size.height * 0.045), controlPoint2: CGPoint(x: size.width * 0.215, y: size.height * 0.365))
-        tlSparkPath.addCurve(to: CGPoint(x: size.width * 0.315, y: size.height * 0.295), controlPoint1: CGPoint(x: size.width * 0.215, y: size.height * 0.345), controlPoint2: CGPoint(x: size.width * 0.315, y: size.height * 0.295))
-        tlSparkPath.close()
-        tlSparkPath.fill()
-
-        blSparkPath.move(to: CGPoint(x: size.width * 0.315, y: size.height * 0.705))
-        blSparkPath.addLine(to: CGPoint(x: size.width * 0.365, y: size.height * 0.805))
-        blSparkPath.addLine(to: CGPoint(x: size.width * 0.065, y: size.height * 0.955))
-        blSparkPath.addCurve(to: CGPoint(x: size.width * 0.215, y: size.height * 0.645), controlPoint1: CGPoint(x: size.width * 0.065, y: size.height * 0.955), controlPoint2: CGPoint(x: size.width * 0.215, y: size.height * 0.645))
-        blSparkPath.addCurve(to: CGPoint(x: size.width * 0.315, y: size.height * 0.705), controlPoint1: CGPoint(x: size.width * 0.215, y: size.height * 0.655), controlPoint2: CGPoint(x: size.width * 0.315, y: size.height * 0.705))
-        blSparkPath.close()
-        blSparkPath.fill()
-
-        starPath.move(to: CGPoint(x: size.width * 0.515, y: size.height * 0.000))
-        starPath.addLine(to: CGPoint(x: size.width * 0.655, y: size.height * 0.345))
-        starPath.addLine(to: CGPoint(x: size.width * 1.005, y: size.height * 0.495))
-        starPath.addLine(to: CGPoint(x: size.width * 0.655, y: size.height * 0.645))
-        starPath.addLine(to: CGPoint(x: size.width * 0.505, y: size.height * 0.995))
-        starPath.addLine(to: CGPoint(x: size.width * 0.355, y: size.height * 0.645))
-        starPath.addLine(to: CGPoint(x: size.width * 0.005, y: size.height * 0.495))
-        starPath.addLine(to: CGPoint(x: size.width * 0.355, y: size.height * 0.345))
-        starPath.addLine(to: CGPoint(x: size.width * 0.515, y: size.height * 0.000))
-        starPath.fill()
+        let leftEye = UIBezierPath(ovalIn: CGRect(
+            x: size.width * 0.42,
+            y: size.height * 0.22,
+            width: size.width * 0.06,
+            height: size.height * 0.06
+        ))
+        UIColor.black.setFill()
+        leftEye.fill()
+        
+        let rightEye = UIBezierPath(ovalIn: CGRect(
+            x: size.width * 0.52,
+            y: size.height * 0.22,
+            width: size.width * 0.06,
+            height: size.height * 0.06
+        ))
+        UIColor.black.setFill()
+        rightEye.fill()
     }
 }
